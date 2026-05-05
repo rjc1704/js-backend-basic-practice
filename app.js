c054ad15503a7cc46e5d4cc857405b639783d03d
Jecheon Ryu <rjc1704@gmail.com>
2026-05-05 20:42:26 +0900

    cors 테스트

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(
-  cors({
-    origin: "https://www.google.com",
-  }),
-);
+app.use(cors());
 app.use(express.json());
 
 connectDB();
